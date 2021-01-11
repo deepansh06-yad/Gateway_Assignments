661be9345f3b2478f771b3e917e3bcbb1803f4b3
deepansh06-yad <72153821+deepansh06-yad@users.noreply.github.com>
2021-01-11 16:59:34 +0530

    Initial and final commit

---
+++ Product Management/Productmanagement.Tests/Controller/ProductManagementTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Http;
+using ProductManagement.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductManagement.Models;
+using ProductManagement;
+
+namespace Productmanagement.Tests.Controllers
+{
+    [TestClass]
+    class ProductManagementTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            productController product = new productController();
+
+            //Act
+            var pc = product.Index(1,"Mi 9 pro");
+
+            Assert.IsNotNull(pc);
+        }
+        [TestMethod]
+        public void ProductList()
+        {
+            ProductdbEntities db = new ProductdbEntities();
+
+            //Act
+            var result = db.Productslists.ToList();
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void CreateorEdit(int id)
+        {
+            productController pro = new productController();
+
+            //Act
+            pro.CreateorEdit(0);
+
+            //Assert
+            
+        }
+    }
+}
