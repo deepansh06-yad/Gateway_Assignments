cbf819fc6531958c8d56899c1c0c23c441515094
deepansh06-yad <72153821+deepansh06-yad@users.noreply.github.com>
2021-03-02 23:20:18 +0530

    Initial commit

---
+++ ExtensionMethodAssignment/ExtensionMethodAssignment/Entensionhelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodAssignment
+{
+    public static class Entensionhelper
+    {
+        public static string CreateUpperCase(string inputstring)
+        {
+            if (inputstring.Length > 0)
+            {
+                char[] chararray = inputstring.ToCharArray();
+                chararray[0] = char.IsUpper(chararray[0]) ? char.ToUpper(chararray[0]) : char.ToUpper(chararray[0]);
+                return new string(chararray);
+            }
+            return inputstring;
+        }
+        public static string CreateLowerCase(string inputstring)
+        {
+            if (inputstring.Length > 0)
+            {
+                char[] chararray = inputstring.ToCharArray();
+                chararray[0] = char.IsLower(chararray[0]) ? char.ToLower(chararray[0]) : char.ToLower(chararray[0]);
+                return new string(chararray);
+            }
+            return inputstring;
+        }
+        public static string Converttoint(string inputstring)
+        {
+            int chararray;
+            if (inputstring.Length>0)
+            {
+                //char[] chararray = inputstring.ToCharArray();
+                chararray = (int)Int64.Parse(inputstring);
+
+            }
+            return chararray;
+        }
+    }
+}
---
+++ ExtensionMethodAssignment/ExtensionMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodAssignment
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string FirstName = "anand";
+            string result = Entensionhelper.CreateUpperCase(FirstName);
+            string resultobj = Entensionhelper.CreateLowerCase(FirstName);
+            string answer = Entensionhelper.Converttoint(FirstName);
+            Console.WriteLine(result);
+            Console.WriteLine(resultobj);
+            Console.WriteLine(answer);
+        }
+    }
+}
