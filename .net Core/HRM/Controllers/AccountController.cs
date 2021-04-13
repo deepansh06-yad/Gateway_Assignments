d5462eaae4f37940f14e77e7f056072e37ecfb1b
= <=>
2021-04-13 14:35:11 +0530

    minor changes

---
+++ .net Core/HRM/Controllers/AccountController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HRM.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string userName, string password)
+        {
+            if (!string.IsNullOrEmpty(userName) && string.IsNullOrEmpty(password))
+            {
+                return RedirectToAction("Login");
+            }
+
+            //Check the user name and password  
+            //Here can be implemented checking logic from the database  
+            ClaimsIdentity identity = null;
+            bool isAuthenticated = false;
+
+            if (userName == "Admin" && password == "password")
+            {
+
+                //Create the identity for the user  
+                    identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Role, "Admin")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticated = true;
+            }
+            if (userName == "John" && password == "john12")
+            {
+
+                //Create the identity for the user  
+                identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Role, "User")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticated = true;
+            }
+            if (userName == "Rich" && password == "rich123")
+            {
+
+                //Create the identity for the user  
+                identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Role, "User")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticated = true;
+            }
+            if (userName == "Riya" && password == "riya124")
+            {
+
+                //Create the identity for the user  
+                identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Role, "User")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticated = true;
+            }
+            if (isAuthenticated)
+                {
+                    var principal = new ClaimsPrincipal(identity);
+
+                    var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    return RedirectToAction("Index", "DashBoard");
+                }
+            
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            var login = HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
+        }
+    }
+}
